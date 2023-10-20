35484ff6e81d4c678467dda0b11052137f7c3da7
ZDMF <fikardavbi12@gmail.com>
2023-10-21 03:06:57 +0700

    add printer function

--- src/simple-webserver.cpp
+++ src/simple-webserver.cpp
@@ -4,8 +4,24 @@
 #include <Husarnet.h>
 #include <WiFi.h>
 #include <Adafruit_Thermal.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #define HTTP_PORT 8080
+
+#define MQTT_TOPIC "dismore/print/rooster"
+#define MQTT_SERVER "192.168.184.212"
+
+char MQTT_CLIENTID[10];
+
+WiFiClient net;
+PubSubClient client(net);
+Adafruit_Thermal printer(&Serial2);
+
+String device_id, table_number, payment_method, invoice_id, service, customer_name, order_date, order_time, no_member, quantity, productname, notes;
+bool clientconnect = true, clientloop = true, wl = true;
+long lastReconnectAttempt, lastReconnectAttempt2;
+
 
 // For GitHub Actions OTA deploment
 
@@ -24,7 +40,7 @@ const char *hostName = "dismoreprinter1";
 extern const char index_html_start[] asm("_binary_src_index_html_start");
 const String index_html = String((const char*)index_html_start);
 
-void setup(void) {
+void ota_init(){
   // ===============================================
   // Wi-Fi, OTA and Husarnet VPN configuration
   // ===============================================
@@ -96,9 +112,6 @@ void setup(void) {
   // ===============================================
 
   // Example webserver hosting table with known Husarnet Hosts
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/html", index_html);
-  });
 
   Serial1.println("🚀 HTTP server started\r\n");
   Serial1.printf("Visit:\r\nhttp://%s:%d/\r\n\r\n", hostName, HTTP_PORT);
@@ -107,12 +120,216 @@ void setup(void) {
   for (auto const &host : Husarnet.listPeers()) {
     Serial1.printf("%s (%s)\r\n", host.second.c_str(), host.first.toString().c_str());
   }
+}
+
+void print_data1() {
+  printer.boldOn();
+  printer.justify('L');
+  printer.setSize('L');
+  printer.println(table_number);
+  //
+  printer.justify('R');
+  printer.setSize('S');
+  printer.println("Payment by");
+  //
+  printer.boldOff();
+  printer.println(payment_method);
+  //
+  printer.justify('L');
+  printer.boldOn();
+  printer.println("Invoice: " + invoice_id);
+  printer.boldOff();
+  //
+  printer.println("");
+  yield();
+  //
+  printer.print("Service ");
+  printer.boldOn();
+  printer.println("\t\t" + service);
+  printer.boldOff();
+  //
+  printer.print("Date ");
+  printer.boldOn();
+  //
+  printer.println("\t\t\t" + order_date);
+  printer.boldOff();
+  //
+  printer.print("Customer ");
+  printer.boldOn();
+  printer.println("\t\t" + customer_name);
+  printer.boldOff();
+  //
+  printer.print("Order Time ");
+  printer.boldOn();
+  printer.println("\t\t" + order_time);
+  printer.boldOff();
+  //
+  printer.justify('C');
+  printer.println("--------------------------------");
+  //
+  printer.justify('L');
+  printer.boldOn();
+  printer.setSize('S');
+  printer.println("Order Details");
+  printer.boldOff();
+  //
+  printer.println("");
+  //
+}
+
+void print_data2() {
+  printer.justify('L');
+  printer.boldOn();
+  printer.setSize('S');
+  printer.println(productname);
+  printer.justify('R');
+  printer.println("x" + quantity);
+  printer.boldOff();
+  //
+  if (notes != "") printer.println(notes);
+}
+
+void print_data3() {
+  printer.justify('C');
+  printer.println("--------------------------------");
+  //
+  printer.justify('L');
+  printer.println("Member");
+  printer.justify('R');
+  printer.println(no_member);
+  //
+  printer.justify('C');
+  printer.println("--------------------------------");
+  //
+  printer.println("");
+  printer.println("");
+  printer.println("");
+}
+
+void messageHandler(char* topic, byte* payload, unsigned int length) {
+  String payloadStr = "";
+  for (unsigned int i = 1; i < length - 1; i++) {
+    if ((char)payload[i] != 92) {
+      payloadStr += (char)payload[i];
+      Serial.print((char)payload[i]);
+    }
+  }
+  payloadStr.trim();
+  StaticJsonDocument<4096> doc;
+  DeserializationError error = deserializeJson(doc, payloadStr);
+  if (error) {
+    Serial1.print("JSON parsing error: ");
+    Serial1.println(error.c_str());
+    return;
+  }
+  //if (printer.hasPaper()) {
+  const JsonArray& order_details = doc["order_details"];
+  device_id = doc["device_id"].as<String>();
+  table_number = doc["table_number"].as<String>();
+  payment_method = doc["payment_method"].as<String>();
+  invoice_id = doc["invoice_id"].as<String>();
+  service = doc["service"].as<String>();
+  customer_name = doc["customer_name"].as<String>();
+  order_date = doc["order_date"].as<String>();
+  order_time = doc["order_time"].as<String>();
+  no_member = doc["no_member"].as<String>();
+  for (const JsonObject& product : order_details) {
+    productname = product["product_name"].as<String>();
+    quantity = product["quantity"].as<String>();
+    notes = product["notes"].as<String>();
+  }
+  Serial1.println("sukses bang");
+  /*if (printer.hasPaper()) {
+    //print_data1();
+    //print_data2();
+    p//rint_data3();
+  }*/
+  //}
+}
+
+void setupmqtt() {
+  client.setServer(MQTT_SERVER, 1883);
+  client.setBufferSize(4096);
+  client.setCallback(messageHandler);
+
+  Serial1.println("Connecting to MQTT");
+
+  while (!client.connect(MQTT_CLIENTID, NULL, NULL, NULL, 1, 0, NULL, 0)) {
+    Serial1.print(".");
+    delay(100);
+  }
+  if (!client.connected()) {
+    Serial1.println("Connect Timeout!");
+    return;
+  } else {
+    digitalWrite(12, HIGH);
+  }
+
+  client.subscribe(MQTT_TOPIC, 1);
+  Serial1.println("MQTT Connected!");
+}
+
+boolean reconnect() {
+  if (client.connect(MQTT_CLIENTID, NULL, NULL, NULL, 1, 0, NULL, 0)) {
+    client.subscribe(MQTT_TOPIC, 1);
+  }
+  return client.connected();
+}
+
+void setup(void) {
+  ota_init();
+  sprintf(MQTT_CLIENTID, "%s", "node1");
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+  pinMode(12, OUTPUT);
+  printer.begin();
+  printer.setTimes(1500, 1500);
+  setupmqtt();
+  lastReconnectAttempt = 0;
+  lastReconnectAttempt2 = 0;
+  Serial1.print(MQTT_CLIENTID);
   pinMode(2,OUTPUT);
 }
 
 void loop(void) {
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
+  //if (printer.hasPaper()) {
+  if (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(12, LOW);
+    if (!WiFi.isConnected()) setupmqtt();
+    wl = false;
+  } else {
+    wl = true;
+  }
+
+  if (!client.connected()) {
+    long now = millis();
+    digitalWrite(12, LOW);
+    clientconnect = false;
+    if (now - lastReconnectAttempt > 1000) {
+      lastReconnectAttempt = now;
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    clientconnect = true;
+  }
+
+  if (!client.loop()) {
+    digitalWrite(12, LOW);
+    long now_ = millis();
+    clientloop = false;
+    if (now_ - lastReconnectAttempt2 > 1000) {
+      lastReconnectAttempt2 = now_;
+      if (reconnect()) {
+        lastReconnectAttempt2 = 0;
+      }
+    }
+  } else {
+    clientloop = true;
+  }
+
+  if (clientloop && clientconnect && wl) {
+    digitalWrite(12, HIGH);
+  }
 }
